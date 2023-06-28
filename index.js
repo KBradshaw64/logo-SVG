acb6af666c075fffe89e96b22285fad472ebdf6c
KBradshaw64 <130187631+KBradshaw64@users.noreply.github.com>
2023-06-28 16:44:55 -0700

    refactored previous code - filled out main logo function - added triangle and square classes

--- index.js
+++ index.js
@@ -1,63 +1,62 @@
 //const shapesChoice = require('./lib/Shape');
 //const shapesTest = require('./lib/shapes.test');
 const inquirer = require('inquirer');
-const fs = require('fs')
-const {Circle, Triangle} = require('./lib/Shape')
+const fs = require('fs');
+const {Circle, Triangle, Square} = require('./lib/Shape');
 
+const questions = [
+{
+    message: "What shape would you like the logo to be?",
+    type: "list",
+    choices: ['Circle', 'Triangle', 'Square'],
+    name: 'shape'
+},
+{
+    message: "What color would you like the shape to be?",
+    type: "input",
+    name: 'shapeColor'
+},
+{
+    message: "What three letters should be in the logo?",
+    type: "input",
+    name: 'text'
+},
+{
+    message: "What color would you like the text to be?",
+    type: "input",
+    name: 'textColor'
+}];
 
-inquirer.prompt([
-    {
-        message: "what the shape?",
-        type: "list",
-        choices: ['Circle', 'Triangle'],
-        name: 'shape'
-
-    },
-    {
-        message: "what the color of the shape",
-        type: "input",
-
-        name: 'shapeColor'
-
-    },
-    {
-        message: "what the color of the text",
-        type: "input",
-
-        name: 'textColor'
-
-    },
-    {
-        message: "what the text",
-        type: "input",
-
-        name: 'text'
-
-    },
-
-
-]).then(
-
-    answer => {
-
-
+inquirer.prompt(questions).then(answer => {
         if (answer.shape == "Circle") {
-
             const newShape = new Circle(answer.shapeColor);
             const element = newShape.render();
-            const svg = `
-        <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-
-        ${element}
-
-        <text x="150" y="125" font-size="60" text-anchor="middle" fill="${answer.textColor}">${answer.text}</text>
-
-        </svg>`
-
+            const svg = 
+                `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+                ${element}
+                <text x="150" y="125" font-size="60" text-anchor="middle" fill="${answer.textColor}">${answer.text}</text>
+                </svg>`
             fs.writeFileSync('logo.svg', svg)
-
-        } else if (answer.shape == "Triangle") {
-            return
+        } 
+        else if (answer.shape == "Triangle") {
+            const newShape = new Triangle(answer.shapeColor);
+            const element = newShape.render();
+            const svg = 
+                `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+                ${element}
+                <text x="150" y="125" font-size="60" text-anchor="middle" fill="${answer.textColor}">${answer.text}</text>
+                </svg>`
+            fs.writeFileSync('logo.svg', svg);
+        }
+        else {
+            const newShape = new Square(answer.shapeColor);
+            const element = newShape.render();
+            const svg = 
+                `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+                ${element}
+                <text x="150" y="125" font-size="60" text-anchor="middle" fill="${answer.textColor}">${answer.text}</text>
+                </svg>`
+            fs.writeFileSync('logo.svg', svg);
         }
     }
 )
